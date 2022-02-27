891ad898059b0d997082d19f52691e327cfd9b1b
Dima Zap <zapilenovd@gmail.com>
2022-02-27 14:28:20 +0300

    Test Driver

--- src/test/java/ru/netology/CardRegTest.java
+++ src/test/java/ru/netology/CardRegTest.java
@@ -7,20 +7,23 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-class CardRegTest {
-    private WebDriver driver;
+class ChromeTest {
+
+        WebDriver driver;
 
     @BeforeAll
-    static void setUpAll(){
+    static void setupClass() {
         WebDriverManager.chromedriver().setup();
     }
+
     @BeforeEach
-    void steUP(){
+    void setupTest(){
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--no-sandbox");
@@ -29,19 +32,20 @@ class CardRegTest {
     }
 
     @AfterEach
-    void tearDown() {
+    void teardown() {
         driver.quit();
         driver = null;
     }
     @Test
     void shouldTest(){
         driver.get("http://localhost:9999");
+        WebElement form = driver.findElement(By.cssSelector("[data-test-id=name]"));
         driver.findElement(By.cssSelector("[type='text']")).sendKeys("Ольга Вениаминовна");
         driver.findElement(By.cssSelector("[type='tel']")).sendKeys("+79096855050");
         driver.findElement(By.className("checkbox__box")).click();
         driver.findElement(By.tagName("button")).click();
         String actual = driver.findElement(By.cssSelector("[data-test-id='order-success']")).getText().trim();
-        String excpected = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время";
-        assertEquals(excpected,actual);
+        String expected = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время";
+        assertEquals(expected,actual);
     }
 }
